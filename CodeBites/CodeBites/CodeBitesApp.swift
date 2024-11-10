85520833fcf1817306e6653a90ada9ddded89164
Jack <jack.gregory.scallan@gmail.com>
2024-11-10 12:37:24 -0500

    Initialized app

---
+++ CodeBites/CodeBites/CodeBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBitesApp.swift
+//  CodeBites
+//
+//  Created by Jack Scallan on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBites/CodeBites/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CodeBites
+//
+//  Created by Jack Scallan on 11/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
